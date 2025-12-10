385117e3934a4c201d1fbbcfac654c8d2803c44b
KELECHIDAVIS <kboss16058@gmail.com>
2025-12-10 13:14:52 -0800

    Working on fen parsing

---
+++ build/CMakeFiles/Pufferfish.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pufferfish.dir/main.c.o"
+  "CMakeFiles/Pufferfish.dir/main.c.o.d"
+  "Pufferfish"
+  "Pufferfish.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Pufferfish.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Pufferfish.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Pufferfish.
+# This may be replaced when dependencies are built.
