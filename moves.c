8233402a0be5641ec557b183f5c18f038da75028
KELECHIDAVIS <kboss16058@gmail.com>
2025-12-11 11:23:09 -0800

    Starting on knight movements

---
+++ moves.c
@@ -0,0 +1,7 @@
+#include "moves.h"
+
+
+// to be the most efficient, this is going to be look up based 
+U64 getKnightMoves(U64 knightPositions){
+
+}
---
+++ moves.h
@@ -0,0 +1,10 @@
+#ifndef MOVES_H
+#define MOVES_H
+
+#include "board.h"
+
+extern 
+U64 getKnightMoves (U64 knightPositions); 
+
+
+#endif //MOVES_H
