2c549446235965ab09e1601af8c30b6af953fe09
Kelechi Duru <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-12-15 12:11:14 -0800

    Running the code to precompute and save the magic numbers to a file.

--- src/main.c
+++ src/main.c
@@ -1,26 +1,73 @@
-
 #include "moves.h"
 #include "attack_patterns.h"
+
 int main()
 {
-    // for each square, generate the magic number
+    // Generate Bishop Magic Numbers
+    printf("Generating Bishop Magic Numbers...\n");
     precomputeBishopMasks();
-    FILE* fp = fopen("bishop_magic_structs.txt", "w"); 
-    fprintf(fp , "{\n"); 
+
+    FILE *fp = fopen("bishop_magic_structs.txt", "w");
+    if (!fp)
+    {
+        fprintf(stderr, "Error: Could not open bishop_magic_structs.txt for writing\n");
+        return 1;
+    }
+
+    fprintf(fp, "{\n");
     for (int square = 0; square < 64; square++)
-    { 
-        printf("Square %d\n", square); 
-        // Bishops
-        // shift amt is determined by the number of bits the relevant blocker has at this square then subtract that from 64
+    {
         int bitsSet = __builtin_popcountll(BishopMagicTable[square].mask);
         int shiftAmt = 64 - bitsSet;
         findMagicNum(true, square, shiftAmt);
 
-        // save SMagic struct for this square to file {mask , magic , shiftAmt}
-        fprintf(fp, "{ %llx, %llx, %d }", BishopMagicTable[square].mask, BishopMagicTable[square].magic, BishopMagicTable[square].shiftAmt) ; 
-        if (square <63) fprintf(fp, ",\n"); 
+        printf("Square %2d: { 0x%016llx, 0x%016llx, %d }\n",
+               square, BishopMagicTable[square].mask,
+               BishopMagicTable[square].magic,
+               BishopMagicTable[square].shiftAmt);
+
+        fprintf(fp, "    { 0x%016llxULL, 0x%016llxULL, %d }%s\n",
+                BishopMagicTable[square].mask,
+                BishopMagicTable[square].magic,
+                BishopMagicTable[square].shiftAmt,
+                square < 63 ? "," : "");
     }
-    fprintf(fp, "\n}"); 
+    fprintf(fp, "};\n");
+    fclose(fp);
+    printf("Bishop magic numbers saved to bishop_magic_structs.txt\n\n");
+
+    // Generate Rook Magic Numbers
+    printf("Generating Rook Magic Numbers...\n");
+    precomputeRookMasks();
+
+    FILE *fp2 = fopen("rook_magic_structs.txt", "w");
+    if (!fp2)
+    {
+        fprintf(stderr, "Error: Could not open rook_magic_structs.txt for writing\n");
+        return 1;
+    }
+
+    fprintf(fp2, "{\n");
+    for (int square = 0; square < 64; square++)
+    {
+        int bitsSet = __builtin_popcountll(RookMagicTable[square].mask);
+        int shiftAmt = 64 - bitsSet;
+        findMagicNum(false, square, shiftAmt);
+
+        printf("Square %2d: { 0x%016llx, 0x%016llx, %d }\n",
+               square, RookMagicTable[square].mask,
+               RookMagicTable[square].magic,
+               RookMagicTable[square].shiftAmt);
+
+        fprintf(fp2, "    { 0x%016llxULL, 0x%016llxULL, %d }%s\n",
+                RookMagicTable[square].mask,
+                RookMagicTable[square].magic,
+                RookMagicTable[square].shiftAmt,
+                square < 63 ? "," : "");
+    }
+    fprintf(fp2, "};\n");
+    fclose(fp2);
+    printf("Rook magic numbers saved to rook_magic_structs.txt\n");
 
     return 0;
 }
