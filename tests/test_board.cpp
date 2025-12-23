a637589844d947de0e1da01325257b1460685b93
Kelechi Duru <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-12-23 11:06:26 -0800

    Perfect perft test suite was added and passed!! Can officially move onto search and evaluation

--- tests/test_board.cpp
+++ tests/test_board.cpp
@@ -90,7 +90,7 @@ INSTANTIATE_TEST_SUITE_P(
         PerftTestParams{"Position3_D5", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 5, 674624},
         PerftTestParams{"Position3_D6", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 6, 11030083},
         PerftTestParams{"Position3_D7", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 7, 178633661ULL},
-        PerftTestParams{"Position3_D8", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 8, 3009794393ULL},
+        // PerftTestParams{"Position3_D8", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 8, 3009794393ULL},
 
         // MIRRORED WORKS, FIRST SUGGESTION DOES NOT MAY REVISIT LATER
         PerftTestParams{"Position4_D1", "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 1, 6},
@@ -112,11 +112,53 @@ INSTANTIATE_TEST_SUITE_P(
         PerftTestParams{"Position6_D3", "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 3, 89890},
         PerftTestParams{"Position6_D4", "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 4, 3894594},
         PerftTestParams{"Position6_D5", "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 5, 164075551},
-        PerftTestParams{"Position6_D6", "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 6, 6923051137ULL},
-        PerftTestParams{"Position6_D7", "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 7, 287188994746ULL},
-        PerftTestParams{"Position6_D8", "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 8, 11923589843526ULL},
-        PerftTestParams{"Position6_D9", "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 9, 490154852788714ULL}
-     ),
+        // PerftTestParams{"Position6_D6", "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 6, 6923051137ULL},
+        // PerftTestParams{"Position6_D7", "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 7, 287188994746ULL},
+        // PerftTestParams{"Position6_D8", "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 8, 11923589843526ULL},
+        // PerftTestParams{"Position6_D9", "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 9, 490154852788714ULL}
+
+        // -- -Perfect Perft Test Suite(Edge Cases)-- -
+        // Illegal ep move #1
+        PerftTestParams{"IllegalEP1_D6", "3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1", 6, 1134888},
+
+        // Illegal ep move #2
+        PerftTestParams{"IllegalEP2_D6", "8/8/4k3/8/2p5/8/B2P2K1/8 w - - 0 1", 6, 1015133},
+
+        // EP Capture Checks Opponent
+        PerftTestParams{"EPCaptureChecks_D6", "8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1", 6, 1440467},
+
+        // Short Castling Gives Check
+        PerftTestParams{"ShortCastleCheck_D6", "5k2/8/8/8/8/8/8/4K2R w K - 0 1", 6, 661072},
+
+        // Long Castling Gives Check
+        PerftTestParams{"LongCastleCheck_D6", "3k4/8/8/8/8/8/8/R3K3 w Q - 0 1", 6, 803711},
+
+        // Castle Rights
+        PerftTestParams{"CastleRights_D4", "r3k2r/1b4bq/8/8/8/8/7B/R3K2R w KQkq - 0 1", 4, 1274206},
+
+        // Castling Prevented
+        PerftTestParams{"CastlingPrevented_D4", "r3k2r/8/3Q4/8/8/5q2/8/R3K2R b KQkq - 0 1", 4, 1720476},
+
+        // Promote out of Check
+        PerftTestParams{"PromoteOutOfCheck_D6", "2K2r2/4P3/8/8/8/8/8/3k4 w - - 0 1", 6, 3821001},
+
+        // Discovered Check
+        PerftTestParams{"DiscoveredCheck_D5", "8/8/1P2K3/8/2n5/1q6/8/5k2 b - - 0 1", 5, 1004658},
+
+        // Promote to give check
+        PerftTestParams{"PromoteToCheck_D6", "4k3/1P6/8/8/8/8/K7/8 w - - 0 1", 6, 217342},
+
+        // Under Promote to give check
+        PerftTestParams{"UnderPromoteCheck_D6", "8/P1k5/K7/8/8/8/8/8 w - - 0 1", 6, 92683},
+
+        // Self Stalemate
+        PerftTestParams{"SelfStalemate_D6", "K1k5/8/P7/8/8/8/8/8 w - - 0 1", 6, 2217},
+
+        // Stalemate & Checkmate #1
+        PerftTestParams{"StalemateCheckmate1_D7", "8/k1P5/8/1K6/8/8/8/8 w - - 0 1", 7, 567584},
+
+        // Stalemate & Checkmate #2
+        PerftTestParams{"StalemateCheckmate2_D4", "8/8/2k5/5q2/5n2/8/5K2/8 b - - 0 1", 4, 23527}),
 
     [](const testing::TestParamInfo<PerftTestParams> &info) {
         return info.param.label; // Use the 'label' field as the test name
