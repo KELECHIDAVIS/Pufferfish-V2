21cedec135a0ddad305a937cbdfc094b2401eb0f
Kelechi Duru <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-12-22 19:47:45 -0800

    Perft Position 3 and The mirrored version of position 4 works
    Idk why the non mirrored version doesn't work but oh well

--- tests/test_board.cpp
+++ tests/test_board.cpp
@@ -1,63 +1,54 @@
 #include <gtest/gtest.h>
-#include <vector>
 #include <string>
+#include <vector>
 
-extern "C"
-{
+extern "C" {
+#include "board.h"
 #include "perft.h"
-#include "board.h" 
 }
 
-class ChessEnvironment : public ::testing::Environment
-{
-public:
+class ChessEnvironment : public ::testing::Environment {
+  public:
     // This runs once before all tests in the entire program
-    void SetUp() override
-    {
+    void SetUp() override {
         precomputeAllAttacks();
     }
 
     // This runs once after all tests have finished
-    void TearDown() override
-    {
+    void TearDown() override {
         // Optional: clean up memory if your engine allocates tables on the heap
     }
 };
 
 // Structure to hold our test data
-struct PerftTestParams
-{
+struct PerftTestParams {
     std::string label;  // Name of the position (e.g., "Kiwipete")
     std::string fen;    // The FEN string to set up
     int depth;          // Depth to search
     U64 expected_nodes; // Correct leaf node count
 };
 
-class PerftTest : public ::testing::TestWithParam<PerftTestParams>
-{
-public: 
+class PerftTest : public ::testing::TestWithParam<PerftTestParams> {
+  public:
     // This runs once before the first test in this suite starts
-    static void SetUpTestSuite()
-    {
+    static void SetUpTestSuite() {
         printf("\n[ ENGINE ] Precomputing attack tables...\n");
         precomputeAllAttacks();
     }
 
     // Optional: runs before EVERY individual test
-    void SetUp() override
-    {
+    void SetUp() override {
         // Reset board or other engine state if needed
     }
 };
 
-TEST_P(PerftTest, VerifyMoveGeneration)
-{
+TEST_P(PerftTest, VerifyMoveGeneration) {
     const auto &params = GetParam();
     Board board;
     std::string fen_copy = params.fen;
-    initBoard(&board, &fen_copy[0]); 
+    initBoard(&board, &fen_copy[0]);
 
-    precomputeAllAttacks(); 
+    precomputeAllAttacks();
 
     // 2. Execute perft
     U64 actual_nodes = perft(&board, params.depth);
@@ -88,12 +79,30 @@ INSTANTIATE_TEST_SUITE_P(
         PerftTestParams{"Kiwipete_D3", "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 3, 97862},
         PerftTestParams{"Kiwipete_D4", "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 4, 4085603},
         PerftTestParams{"Kiwipete_D5", "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 5, 193690690},
-        PerftTestParams{"Kiwipete_D6", "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 6, 8031647685}),
+        /*TODO:  THIS ONE GIVES ERROR MAY COME BACK TO IT LATER  */
+        PerftTestParams{"Kiwipete_D6", "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 6, 8031647685ULL},
 
-    [](const testing::TestParamInfo<PerftTestParams> &info)
-    {
+        /* Next few positions are from : https://www.chessprogramming.org/Perft_Results */
+        PerftTestParams{"Position3_D1", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 1, 14},
+        PerftTestParams{"Position3_D2", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 2, 191},
+        PerftTestParams{"Position3_D3", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 3, 2812},
+        PerftTestParams{"Position3_D4", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 4, 43238},
+        PerftTestParams{"Position3_D5", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 5, 674624},
+        PerftTestParams{"Position3_D6", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 6, 11030083},
+        PerftTestParams{"Position3_D7", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 7, 178633661ULL},
+        PerftTestParams{"Position3_D8", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 8, 3009794393ULL},
+
+        PerftTestParams{"Position4_D1", "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 1, 6},
+        PerftTestParams{"Position4_D2", "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 2, 264},
+        PerftTestParams{"Position4_D3", "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 3, 9467},
+        PerftTestParams{"Position4_D4", "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 4, 422333},
+        PerftTestParams{"Position4_D5", "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 5, 15833292},
+        PerftTestParams{"Position4_D6", "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 6, 706045033ULL}
+
+        ),
+
+    [](const testing::TestParamInfo<PerftTestParams> &info) {
         return info.param.label; // Use the 'label' field as the test name
     }
 
 );
-
